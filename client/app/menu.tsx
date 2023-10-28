35810f56fe2cdd1c2e6bf911709c05a7ef58b387
Mario Galeno <galeno.mario@gmail.com>
2023-10-28 12:01:20 -0400

    wip: add menu items

--- client/app/menu.tsx
+++ client/app/menu.tsx
@@ -1,8 +1,40 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, FlatList } from 'react-native';
 import { useQuery } from '@tanstack/react-query';
 import { fetchMenu } from '../api';
 import { Redirect } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Avatar, Card, Text, Button } from 'react-native-paper';
+
+const DUMMY_IMG = 'https://picsum.photos/700';
+const LeftContent = (props: any) => <Avatar.Icon {...props} icon='food' />;
+
+const MenuItem = ({ item }: any) => {
+  const { dish, description, price } = item;
+  const currencyFormatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+  });
+
+  return (
+    <Card>
+      <Card.Title
+        title={dish}
+        subtitle={currencyFormatter.format(price)}
+        left={LeftContent}
+      ></Card.Title>
+      <Card.Content>
+        <Text variant='titleLarge'>{dish}</Text>
+        <Text variant='bodyMedium'>{description}</Text>
+      </Card.Content>
+      <Card.Cover source={{ uri: DUMMY_IMG }} />
+      <Card.Actions>
+        <Button>Add to cart</Button>
+      </Card.Actions>
+    </Card>
+  );
+};
 
 export default function Menu() {
   const { data, error } = useQuery({
@@ -15,13 +47,18 @@ export default function Menu() {
   }
 
   if (data) {
-    return <View />;
+    return (
+      <SafeAreaView>
+        <FlatList data={data} renderItem={MenuItem} style={styles.flatList} />
+      </SafeAreaView>
+    );
   }
 
   return <View />;
 }
 
 const styles = StyleSheet.create({
+  flatList: { flex: 1, padding: 20 },
   spacing: { width: '40%', marginTop: 25 },
   fillAndCenter: {
     justifyContent: 'center',
